df396458bbaa62a25c599324ca595f5ce51b7664
SantosDiv <diogenesfelipe.s@outlook.com>
2020-11-26 15:49:45 -0300

    Resoluçao do item 3 - media aritimetica

--- exercises/4.2/scriptMediaAritimetica.js
+++ exercises/4.2/scriptMediaAritimetica.js
@@ -1,6 +1,6 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let soma = 0, media;
 for( i of numbers ) {
   soma = soma + i;
 }
-console.log(soma);
+console.log(media =  soma/numbers.length);
