52a17db534fa5a567a2a855a167a9ea18bea5d00
SantosDiv <diogenesfelipe.s@outlook.com>
2020-11-26 15:37:13 -0300

    Resoluçao do item 2 - somatorio de array

--- exercises/4.2/scriptSomaArray.js
+++ exercises/4.2/scriptSomaArray.js
@@ -1,6 +1,7 @@
-  // imprimindo todos os valores
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for( i of numbers ) {
-  console.log(i);
-}
+let soma = 0;
 
+for( i of numbers ) {
+  soma = soma + i;
+}
+console.log(soma);
