49cf034a6d2039c07c819c41121e0bda47354dae
SantosDiv <diogenesfelipe.s@outlook.com>
2020-11-30 18:17:20 -0300

    Resoluçao do item 5 - Parte 1

--- exercises/4.4/exercisesPart1.js
+++ exercises/4.4/exercisesPart1.js
@@ -4,10 +4,22 @@ let informations = {
     note: 'Estudante da Trybe e leitor de tolken',
 };
 
-informations.recurrent = 'yes';
-
-for(key in informations) {
-    console.log(informations[key]);
+let informations2 = {
+    persona: 'Tio Patinhas', 
+    origen: 'Christmas on Bear Mountain, Dell\'s Four Color Comics #178',
+    note: 'O último MacPatinhas',
+    recurrent:true,
 }
+informations.recurrent = true;
+
+console.log(informations.persona, 'e', informations2.persona);
+console.log(informations.origen, 'e', informations2.origen);
+console.log(informations.note, 'e', informations2.note);
+if(informations.recurrent && informations2.recurrent){
+    console.log('Ambos recorrentes');
+}
+// for(key in informations) {
+//     console.log(informations[key]);
+// } - item 3 e 4
 // console.log('Bem vindo,', informations.persona); - item 1
 // console.log(informations); - item 2
