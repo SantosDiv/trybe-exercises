670883bd17d9ce58e0ad1d6ffe7d6c3b90845b69
SantosDiv <diogenesfelipe.s@outlook.com>
2021-01-06 15:23:06 -0300

    Adiçao dos arquivos da bibli. just-validate e suas especificaçoes

--- exercises/6.1/script.js
+++ exercises/6.1/script.js
@@ -64,9 +64,31 @@ function fillDropDownState() {
 
 window.onload = function () {
   const buttonSend = document.querySelector("#send");
+  const yearActual = new Date().getFullYear(); 
+  var picker = new Pikaday({ 
+    field: document.getElementById('datepicker'),
+    format: 'D/MM/YYYY',
+    toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+    },
+    parse(dateString, format) {
+        // dateString is the result of `toString` method
+        const parts = dateString.split('/');
+        const day = parseInt(parts[0], 10);
+        const month = parseInt(parts[1], 10) - 1;
+        const year = parseInt(parts[2], 10);
+        return new Date(year, month, day);
+    },
+    yearRange: [1900, yearActual]
+  });
   fillDropDownState();
   buttonSend.addEventListener("click", function (event) {
     event.preventDefault();
-    getElements();
+    // getElements();
   }, false);
 };
