42d357246f05434a2e3dc324033fb16948750a72
Diogenes <diogenesfelipe.s@outlook.com>
2021-01-22 13:21:26 -0300

    Resoluçao do exercicio 7 - bloco 8.4

--- exercises/8.4/exercicio7.js
+++ exercises/8.4/exercicio7.js
@@ -18,6 +18,9 @@ const ships = [
 ]
 
 // escreva shipLength abaixo
+// Usei o object destructuring e o default params para colocar um valor padrão na medida do navio caso
+// não tenha essa propriedade no objeto.
+const shipLength = ({name, length, measurementUnit = 'meters'}) => `${name} is ${length} ${measurementUnit} long`;
 
 assert.strictEqual(shipLength(ships[0]), "Titanic is 269.1 meters long")
 assert.strictEqual(shipLength(ships[1]), "Queen Mary 2 is 1132 feet long")
